d6e396d9505a9e8b3d956b5edd0275659b4436d7
yutaka-osawa <150046340+yutaka-osawa@users.noreply.github.com>
2024-02-17 00:46:15 +0900

    restapiプロジェクト作成

---
+++ my-expenses-restapi/src/main/java/com/expenses/MyExpensesRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyExpensesRestapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyExpensesRestapiApplication.class, args);
+    }
+
+}
