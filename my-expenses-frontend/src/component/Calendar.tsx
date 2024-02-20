729c1f8ef9baebb0401a89bb7bc941bfd0606b9a
yutaka-osawa <150046340+yutaka-osawa@users.noreply.github.com>
2024-02-20 23:53:08 +0900

    カレンダーの日本語化と週の表示の追加

--- my-expenses-frontend/src/component/Calendar.tsx
+++ my-expenses-frontend/src/component/Calendar.tsx
@@ -2,11 +2,14 @@
 
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import jaLocale from '@fullcalendar/core/locales/ja';
 
 const Calendar = () => {
     return (
         <div>
-            <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+            <FullCalendar plugins={[dayGridPlugin, timeGridPlugin]} initialView="dayGridMonth" locales={[jaLocale]} locale={'ja'}
+                          headerToolbar={{left: 'prev,next today', center: 'title', right: 'dayGridMonth,timeGridWeek',}}/>
         </div>
     );
 };
