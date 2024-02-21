7950a0a9ca3b6c49d076d5bdd43a9ff13c98c465
yutaka-osawa <150046340+yutaka-osawa@users.noreply.github.com>
2024-02-21 23:34:12 +0900

    DateClickコンポーネントの作成

--- my-expenses-frontend/src/component/Calendar.tsx
+++ my-expenses-frontend/src/component/Calendar.tsx
@@ -1,17 +1,28 @@
-'use client'
+"use client";
 
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from '@fullcalendar/daygrid';
-import timeGridPlugin from '@fullcalendar/timegrid';
-import jaLocale from '@fullcalendar/core/locales/ja';
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import jaLocale from "@fullcalendar/core/locales/ja";
+import DateClick from "@/component/DateClick";
 
 const Calendar = () => {
-    return (
-        <div>
-            <FullCalendar plugins={[dayGridPlugin, timeGridPlugin]} initialView="dayGridMonth" locales={[jaLocale]} locale={'ja'}
-                          headerToolbar={{left: 'prev,next today', center: 'title', right: 'dayGridMonth,timeGridWeek',}}/>
-        </div>
-    );
+  return (
+    <div>
+      <FullCalendar
+        plugins={[dayGridPlugin, timeGridPlugin]}
+        initialView="dayGridMonth"
+        locales={[jaLocale]}
+        locale={"ja"}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek",
+        }}
+        dateClick={DateClick}
+      />
+    </div>
+  );
 };
 
 export default Calendar;
---
+++ my-expenses-frontend/src/component/DateClick.tsx
@@ -0,0 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
+type DateClickArg = {
+  date: Date; // クリックされた日付
+  dateStr: string; // クリックされた日付の文字列表現 (ISO 8601 形式)
+  allDay: boolean; // イベントが終日かどうか
+  jsEvent: MouseEvent; // ブラウザのマウスイベント
+  // view: View; // カレンダーの表示モード
+};
+const DateClick = (info: DateClickArg) => {
+  const dateStr = info.dateStr;
+  const navigate = useNavigate();
+
+  navigate(`/date/${dateStr}`);
+
+  return (
+    <div>navigate(`/date/${dateStr}`);</div>
+    // ページ遷移先
+  );
+};
+
+export default DateClick;
