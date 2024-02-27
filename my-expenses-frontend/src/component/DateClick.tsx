b727cd133a7e178a4cfaf586b836d32752ede2f2
yutaka-osawa <150046340+yutaka-osawa@users.noreply.github.com>
2024-02-27 22:59:22 +0900

    DateClickコンポーネントの修正

--- my-expenses-frontend/src/component/DateClick.tsx
+++ my-expenses-frontend/src/component/DateClick.tsx
@@ -1,14 +1,14 @@
 import { useNavigate } from "react-router-dom";
 
-type DateClickArg = {
-  date: Date; // クリックされた日付
-  dateStr: string; // クリックされた日付の文字列表現 (ISO 8601 形式)
-  allDay: boolean; // イベントが終日かどうか
-  jsEvent: MouseEvent; // ブラウザのマウスイベント
-  // view: View; // カレンダーの表示モード
-};
-const DateClick = (info: DateClickArg) => {
-  const dateStr = info.dateStr;
+// type DateClickArg = {
+//   date: Date; // クリックされた日付
+//   dateStr: string; // クリックされた日付の文字列表現 (ISO 8601 形式)
+//   allDay: boolean; // イベントが終日かどうか
+//   jsEvent: MouseEvent; // ブラウザのマウスイベント
+//   // view: View; // カレンダーの表示モード
+// };
+const DateClick = ({ dateStr }: { dateStr: string }) => {
+  // const dateStr = info.dateStr;
   const navigate = useNavigate();
 
   navigate(`/date/${dateStr}`);
